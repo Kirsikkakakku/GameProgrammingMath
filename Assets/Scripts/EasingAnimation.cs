91ae238bfb94b8051f8f6b1d07dbdf1bfa0b8ca9
Kirsikkakakku <asaha020@edu.xamk.fi>
2024-01-27 18:45:57 +0200

    Homework 2

--- Assets/Scripts/EasingAnimation.cs
+++ Assets/Scripts/EasingAnimation.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static EasingFunction;
 
 public class EasingAnimation : MonoBehaviour
 {
+    private EasingFunction.Function moveEasingFunction;
+    private EasingFunction.Function rotationEasingFunction;
+    public EasingFunction.Ease moveEaseType;
+    public EasingFunction.Ease rotationEaseType;
+    public float recoilTime = 0.2f;
+    public float recoilMoveAmount = 0.2f;
+    public float recoilRotationAmount = 10f;
 
-    private EasingFunction.Function easingFunction;
-    public EasingFunction.Ease easeType;
-
-    Vector3 target = new Vector3(0, 0, 2);
-
+    private Vector3 originalPos;
+    private Vector3 originalRot;
+    private Vector3 moveTarget;
+    private Vector3 rotTarget;
     private float cooldown = 0.5f;
     private float timeSinceLastShot = 0f;
 
-    private float time = 0f;
+    private float elapsedTime = 0f;
+    private bool recoiling = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalPos = transform.position;
+        originalRot = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
@@ -27,43 +35,48 @@ public class EasingAnimation : MonoBehaviour
     {
         timeSinceLastShot += Time.deltaTime;
 
-        EasingFunction.Function func = GetEasingFunction(easeType);
-
-        if (Vector3.Distance(transform.position, target) <= 0.1f)
+        if (recoiling)
         {
-            transform.position = new Vector3(0, 0, 0);
-            time = 0f;
+            Recoil();
+            return;
         }
 
-        
-        float value = func(0, target.z, time/3);
-
-        time += Time.deltaTime;
-        
-        //EasingFunction.Function derivativeFunc = GetEasingFunctionDerivative(ease);
-        
-        //float derivativeValue = derivativeFunc(0, 0.01f, 0.01f);
-
-
-        transform.position = new Vector3(0, 0, value);
-
-        //easingFunction = 
-
-        /*if (Input.GetKeyDown(KeyCode.Mouse0) && timeSinceLastShot >= cooldown)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timeSinceLastShot >= cooldown && recoiling == false)
         {
-            Shoot();
-        }*/
-    }
+            recoiling = true;
+            originalPos = transform.position;
+            originalRot = transform.rotation.eulerAngles;
+            Debug.Log(originalRot);
+            moveTarget = new Vector3(0, 0, transform.position.z - recoilMoveAmount);
+            rotTarget = new Vector3(transform.rotation.eulerAngles.x - recoilRotationAmount, 0, 0);
+            Debug.Log(rotTarget);
+            elapsedTime = 0f;
+            Recoil();
+        }
 
-    private void Shoot()
-    {
-        timeSinceLastShot = 0f;
-
-        //transform.position = new Vector
     }
 
     private void Recoil()
     {
+        moveEasingFunction = EasingFunction.GetEasingFunction(moveEaseType);
+        rotationEasingFunction = EasingFunction.GetEasingFunction(rotationEaseType);
 
+        if (elapsedTime >= recoilTime)
+        {
+            recoiling = false;
+        }
+
+        else
+        {
+            elapsedTime += Time.deltaTime;
+
+            float t = elapsedTime / recoilTime;
+
+            float moveValue = moveEasingFunction(originalPos.z, moveTarget.z, t);
+            float rotValue = rotationEasingFunction(originalRot.x, rotTarget.x, t);
+
+            transform.position = new Vector3(0, 0, moveValue);
+            transform.eulerAngles = new Vector3(rotValue, 0, 0);
+        }
     }
 }
