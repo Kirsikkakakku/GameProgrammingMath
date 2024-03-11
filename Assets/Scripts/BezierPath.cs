49850458b3acd37b0b87110c5c48c6962a042ad0
Kirsikkakakku <48278250+Kirsikkakakku@users.noreply.github.com>
2024-03-11 15:32:08 +0200

    .

--- Assets/Scripts/BezierPath.cs
+++ Assets/Scripts/BezierPath.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using System.Linq;
+using UnityEditor.Performance.ProfileAnalyzer;
 
 public class BezierPath : MonoBehaviour
 {
@@ -12,18 +13,28 @@ public class BezierPath : MonoBehaviour
 
     [SerializeField] BezierPoint[] points;
     [SerializeField] GameObject obj;
+    [SerializeField] GameObject obj1;
+
+
 
     private Vector3 point;
     private Quaternion rotation;
 
-    private Vector3 anchor1, anchor2, anchor3, anchor4;
+    private Vector3 anchor1, anchor2, anchor3, anchor4, anchorNext1, anchorNext2, anchorNext3, anchorNext4;
+
+    [Range(3, 255)]
+    public int segments = 32;
 
     public bool closed = false;
 
     [Range(0, 1)]
     public float t = 0f;
 
+    public float roadScale = 1f;
+
     private int x = 1;
+
+    private int counter = 0;
 
     OrientedPoint GetOrientedPoint(float t, Vector3 anc1, Vector3 ctrl1, Vector3 ctrl2, Vector3 anc2)
     {
@@ -73,6 +84,7 @@ public class BezierPath : MonoBehaviour
     private void OnDrawGizmos()
     {
         int n = points.Length;
+        int nSegments = n - 1;
 
         for (int i = 0; i < n - 1; i++)
         {
@@ -96,31 +108,93 @@ public class BezierPath : MonoBehaviour
             Handles.DrawBezier(firstAnchor, secondAnchor, firstControl, secondControl, Color.green, null, 3);
         }
 
-        anchor1 = points[0].GetAnchorPoint();
-        anchor2 = points[0].GetSecondControlPoint();
-        anchor3 = points[1].GetFirstControlPoint();
-        anchor4 = points[1].GetAnchorPoint();
-
-        OrientedPoint op = GetOrientedPoint(t, anchor1, anchor2, anchor3, anchor4);
-
-        obj.transform.position = op.Position;
-        obj.transform.rotation = op.Rotation;
-
-        Handles.PositionHandle(op.Position, op.Rotation);
-
-        void DrawPoint(Vector2 localPos) => Gizmos.DrawSphere(op.LocalToWorld(localPos), 0.3f);
-
-        Vector3[] verts = shape2D.vertices.Select(v => op.LocalToWorld(v.point)).ToArray();
-
-        for(int i = 0; i < shape2D.lineIndices.Length; i+=2)
+        for (int segment = 0; segment <= segments-1; segment++)
         {
-            Vector3 a = verts[shape2D.lineIndices[i]];
-            Vector3 b = verts[shape2D.lineIndices[i+1]];
-            Gizmos.color = Color.white;
-            Gizmos.DrawLine(a, b);
-            Gizmos.color = Color.red;
-            DrawPoint(shape2D.vertices[i].point);
-            DrawPoint(shape2D.vertices[i+1].point);
+
+            float TSegment = (float)segment / segments;
+            float TSegmentNext = (float)(segment + 1 )/ segments;
+
+            int segStart = Mathf.FloorToInt(TSegment * n);
+            int segStartNext = Mathf.FloorToInt(TSegmentNext * n);
+
+            if (segStart >= n)
+            {
+                segStart = n - 1;
+            }
+
+            if (segStartNext >= n)
+            {
+                segStartNext = n - 1;
+            }
+
+            float tActual = (float)n * TSegment - segStart * 1.0f;
+            float tActualNext = n * TSegmentNext - segStartNext * 1.0f;
+
+            anchor1 = points[segStart].GetAnchorPoint();
+            anchor2 = points[segStart].GetSecondControlPoint();
+
+            if (segStart + 1 >= n)
+            {
+                anchor3 = points[0].GetFirstControlPoint();
+                anchor4 = points[0].GetAnchorPoint();
+            }
+
+            else
+            {
+                anchor3 = points[segStart + 1].GetFirstControlPoint();
+                anchor4 = points[segStart + 1].GetAnchorPoint();
+            }
+
+            anchorNext1 = points[segStartNext].GetAnchorPoint();
+            anchorNext2 = points[segStartNext].GetSecondControlPoint();
+
+            if (segStartNext + 1 >= n)
+            {
+                anchorNext3 = points[0].GetFirstControlPoint();
+                anchorNext4 = points[0].GetAnchorPoint();
+            }
+
+            else
+            {
+                anchorNext3 = points[segStartNext + 1].GetFirstControlPoint();
+                anchorNext4 = points[segStartNext + 1].GetAnchorPoint();
+            }
+
+            OrientedPoint op = GetOrientedPoint(tActual, anchor1, anchor2, anchor3, anchor4);
+            OrientedPoint opNext = GetOrientedPoint(tActualNext, anchorNext1, anchorNext2, anchorNext3, anchorNext4);
+
+            obj.transform.position = op.Position;
+            obj.transform.rotation = op.Rotation;
+
+            obj1.transform.position = opNext.Position;
+            obj1.transform.rotation = opNext.Rotation;
+
+            //Handles.PositionHandle(op.Position, op.Rotation;
+
+            Vector3[] verts = shape2D.vertices.Select(v => op.LocalToWorld(v.point * roadScale)).ToArray();
+            Vector3[] vertsNext = shape2D.vertices.Select(vNext => opNext.LocalToWorld(vNext.point * roadScale)).ToArray();
+
+            for (int i = 0; i < shape2D.lineIndices.Length; i += 2)
+            {
+                Vector3 a = verts[shape2D.lineIndices[i]];
+                Vector3 b = verts[shape2D.lineIndices[i + 1]];
+                Gizmos.color = Color.white;
+                Gizmos.DrawLine(a, b);
+                Gizmos.color = Color.red;
+            }
+
+            List<Vector3> vertices = new List<Vector3>();
+
+            for (int i = 0; i < verts.Length; i += 2)
+            {
+                vertices.Add(verts[i]);
+                vertices.Add(vertsNext[i]);
+            }
+
+            for (int i = 0; i < vertices.Count; i += 2)
+            {
+                Gizmos.DrawLine(vertices[i], vertices[i + 1]);
+            }
         }
     }
 
